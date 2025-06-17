71ba9ad2a3ebc52b8712e9506f820d8f74be8d67
Aravind Sriramoju <aravindsriramoju@Aravinds-MacBook-Pro.local>
2025-06-17 20:13:15 +0530

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,10 @@ export default defineConfig(({ mode }) => ({
     sourcemap: true,
     rollupOptions: {
       output: {
-        manualChunks: undefined,
+        format: 'es',
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
       },
     },
   },
