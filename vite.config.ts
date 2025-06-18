8144bac84922bbfb9806cdc863101f21b4064ce2
Aravind Sriramoju <aravindsriramoju@Aravinds-MacBook-Pro.local>
2025-06-18 12:23:26 +0530

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
@@ -22,6 +22,7 @@ export default defineConfig(({ mode }) => ({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    emptyOutDir: true,
     sourcemap: true,
     minify: false,
     rollupOptions: {
